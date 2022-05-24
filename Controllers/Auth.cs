5f419496383cf51d53aa5beb06d0f9a21a5e0df1
TomixUG <tomas.sousek1@gmail.com>
2022-05-24 23:49:30 +0200

    add login page

---
+++ Controllers/Auth.cs
@@ -0,0 +1,27 @@
+using DutyFreePraxe.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DutyFreePraxe.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(ILogger<AuthController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
