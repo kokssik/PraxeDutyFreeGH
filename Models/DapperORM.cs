be1e8318958412855f619f32920a610168d87f6b
kokssik <rudolf.sekac@gmail.com>
2022-05-25 14:21:45 +0200

    Added Dapper code

--- Models/DapperORM.cs
+++ Models/DapperORM.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq; 
 using System.Web;
 using Dapper;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace DutyFreePraxe.Models
 {
@@ -13,7 +15,31 @@ namespace DutyFreePraxe.Models
 
         public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param)
         {
+           using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(procedureName,param,commandType: CommandType.StoredProcedure);
+            }
+        }
 
+        //DapperORM.ExecuteReturnScalar<int>(_,_);
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param)
+        {
+           using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return (T)Convert.ChangeType(sqlCon.Execute(procedureName,param,commandType: CommandType.StoredProcedure),typeof(T));
+            }
+        }
+
+        //DapperORM.ReturnList<ProductModel> <= IEnumerable<ProductModel>
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param)
+        {
+           using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(procedureName,param,commandType: CommandType.StoredProcedure);
+            }
         }
 
     }
